1d37c27883f67e781cc1a2f5ea48911d24cb7102
Christian Tzolov <ctzolov@vmware.com>
2024-06-09 08:38:54 +0200

    initial

---
+++ src/main/java/com/example/ai/observability/AiObservabilityDemoApplication.java
@@ -0,0 +1,40 @@
+package com.example.ai.observability;
+
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * https://programmingtechie.com/2023/09/09/spring-boot3-observability-grafana-stack/
+ */
+@SpringBootApplication
+public class AiObservabilityDemoApplication {
+
+	private final Logger logger = LoggerFactory.getLogger(AiObservabilityDemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiObservabilityDemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner test(ChatClient.Builder builder) {
+		ChatClient ai = builder.build();
+		return args -> {
+			for (int i = 0; i < 150; i++) {
+				var response = ai.prompt()
+						.options(OpenAiChatOptions.builder().withSeed(new Random().nextInt()).build())
+						.user("tell me a joke?")
+						.call().chatResponse();
+				logger.info("Response: {}", response.getResult().getOutput().getContent());
+				Thread.sleep(10000);
+			}
+		};
+	}
+}
---
+++ src/test/java/com/example/ai_observability_demo/AiObservabilityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_observability_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiObservabilityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
