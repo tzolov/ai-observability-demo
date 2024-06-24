08de9a1a60ff9b87986c564d2ad0983116fd709b
Christian Tzolov <ctzolov@vmware.com>
2024-06-24 17:48:49 +0200

    add

--- src/main/java/com/example/ai/observability/AiObservabilityDemoApplication.java
+++ src/main/java/com/example/ai/observability/AiObservabilityDemoApplication.java
@@ -9,11 +9,9 @@ import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.observation.aop.ObservedAspect;
+import reactor.core.publisher.Flux;
 
 /**
  * https://programmingtechie.com/2023/09/09/spring-boot3-observability-grafana-stack/
@@ -32,19 +30,28 @@ public class AiObservabilityDemoApplication {
 		ChatClient ai = builder.build();
 		return args -> {
 			for (int i = 0; i < 150; i++) {
-				var response = ai.prompt()	
+				// var response = ai.prompt()	
+				// 		.options(OpenAiChatOptions.builder().withSeed(new Random().nextInt()).build())
+				// 		.user("tell me a joke?")
+				// 		.call().chatResponse();
+				// logger.info("Response: {}", response.getResult().getOutput().getContent());
+
+
+				Flux<String> streamResponse = ai.prompt()	
 						.options(OpenAiChatOptions.builder().withSeed(new Random().nextInt()).build())
 						.user("tell me a joke?")
-						.call().chatResponse();
-				logger.info("Response: {}", response.getResult().getOutput().getContent());
+						.stream().content();
+				
+				streamResponse.collectList().block().stream().forEach(s -> logger.info("Stream Response: {}", s));
+
 				Thread.sleep(10000);
 			}
 		};
 	}
 
-	@Bean
-	@ConditionalOnMissingBean
-	ObservedAspect observedAspect(ObservationRegistry registry) {
-		return new ObservedAspect(registry);
-	}
+	// @Bean
+	// @ConditionalOnMissingBean
+	// ObservedAspect observedAspect(ObservationRegistry registry) {
+	// 	return new ObservedAspect(registry);
+	// }
 }
